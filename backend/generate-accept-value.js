65cf854bac1a2a293953afa57c7ce4f03290ffe1
Aadithya-J <jlaadithya@gmail.com>
2024-06-24 18:57:23 +0530

    established ws connection b/w frontend and backend

--- backend/generate-accept-value.js
+++ backend/generate-accept-value.js
@@ -4,7 +4,6 @@ function generateAcceptValue(acceptKey) {
   return (
     crypto
       .createHash("sha1")
-      // this magic string key is actually in the spec
       .update(acceptKey + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11", "binary")
       .digest("base64")
   );
